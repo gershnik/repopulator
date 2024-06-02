c634675fc584bf055ef3f0164df6bc27d4b15a91
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-06-01 18:50:58 -0700

    Cleaning up metadata

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,7 @@ if (mydir/".extrapythons").exists():
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"] + extraPythons)
 def test(session):
     session.install("pytest")
-    session.install("-r", mydir / "requirements-base.txt")
+    session.install("python-dotenv")
     #session.install("--no-build-isolation", "--editable", ".")
     session.install(".")
     session.run("pytest")
