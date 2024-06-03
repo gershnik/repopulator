da4598e0f7d0b8793fe1b26cea952764f3130ee2
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-06-02 22:16:33 -0700

    Workarounds for Python 3.8 bogus test failures

--- tests/test_pacman.py
+++ tests/test_pacman.py
@@ -1,4 +1,6 @@
 import pytest
+import sys
+import os
 from pathlib import Path
 
 from repopulator import *
@@ -24,13 +26,16 @@ def test_one(binaries_path, output_path, expected_path, pgp_signer, fixed_dateti
     repo.export(output_path, pgp_signer, fixed_datetime, keepExpanded=True)
     compare_files(output_path / 'x86_64/makedumpfile-1.7.1-1-x86_64.pkg.tar.zst', binaries_path / 'makedumpfile-1.7.1-1-x86_64.pkg.tar.zst')
     compare_files(output_path / 'x86_64/makedumpfile-1.7.1-1-x86_64.pkg.tar.zst.sig', binaries_path / 'makedumpfile-1.7.1-1-x86_64.pkg.tar.zst.sig')
-    compare_files(output_path / 'x86_64/fancyrepo.db.tar.gz', expected_path / 'fancyrepo.db.tar.gz', should_populate)
-    compare_files(output_path / 'x86_64/fancyrepo.files.tar.gz', expected_path / 'fancyrepo.files.tar.gz', should_populate)
+    if sys.version_info >= (3, 9):
+        compare_files(output_path / 'x86_64/fancyrepo.db.tar.gz', expected_path / 'fancyrepo.db.tar.gz', should_populate)
+        compare_files(output_path / 'x86_64/fancyrepo.files.tar.gz', expected_path / 'fancyrepo.files.tar.gz', should_populate)
+    compare_files(output_path / 'expanded/x86_64/fancyrepo.db/makedumpfile-1.7.1-1/desc', expected_path / 'desc', should_populate)
+    compare_files(output_path / 'expanded/x86_64/fancyrepo.files/makedumpfile-1.7.1-1/files', expected_path / 'files', should_populate)
     assert (output_path / 'x86_64/fancyrepo.db').is_symlink()
-    assert (output_path / 'x86_64/fancyrepo.db').readlink() == Path('fancyrepo.db.tar.gz')
+    assert os.readlink(output_path / 'x86_64/fancyrepo.db') == 'fancyrepo.db.tar.gz'
     assert (output_path / 'x86_64/fancyrepo.db.sig').is_symlink()
-    assert (output_path / 'x86_64/fancyrepo.db.sig').readlink() == Path('fancyrepo.db.tar.gz.sig')
+    assert os.readlink(output_path / 'x86_64/fancyrepo.db.sig') == 'fancyrepo.db.tar.gz.sig'
     assert (output_path / 'x86_64/fancyrepo.files').is_symlink()
-    assert (output_path / 'x86_64/fancyrepo.files').readlink() == Path('fancyrepo.files.tar.gz')
+    assert os.readlink(output_path / 'x86_64/fancyrepo.files') == 'fancyrepo.files.tar.gz'
     assert (output_path / 'x86_64/fancyrepo.files.sig').is_symlink()
-    assert (output_path / 'x86_64/fancyrepo.files.sig').readlink() == Path('fancyrepo.files.tar.gz.sig')
+    assert os.readlink(output_path / 'x86_64/fancyrepo.files.sig') == 'fancyrepo.files.tar.gz.sig'
