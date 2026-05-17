c810fbf6695e84f6c565ea21f0d190773c2bc0db
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-05-17 10:25:34 -0700

    Comparing repomd.xml properly in tests

--- tests/test_rpm.py
+++ tests/test_rpm.py
@@ -13,7 +13,21 @@ import subprocess
 from repopulator import RpmRepo, RpmVersion
 from repopulator.rpm import _compare_abi_version
 
-from .utils import compare_files
+from .utils import compare_files, compare_xml_files
+
+REPOMD_NORMALIZE = """\
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:repo="http://linux.duke.edu/metadata/repo">
+  <xsl:output method="xml" indent="no"/>
+  <xsl:template match="@*|node()">
+    <xsl:copy><xsl:apply-templates select="@*|node()"/></xsl:copy>
+  </xsl:template>
+  <xsl:template match="repo:data[substring(repo:location/@href, string-length(repo:location/@href) - 2) = '.gz']/repo:checksum/text()"/>
+  <xsl:template match="repo:data[substring(repo:location/@href, string-length(repo:location/@href) - 2) = '.gz']/repo:size/text()"/>
+</xsl:stylesheet>
+"""
 
 
 def test_compareAbiVersion():
@@ -39,7 +53,7 @@ def test_one(binaries_path, output_path, expected_path, pgp_signer, fixed_dateti
     assert package.version_key == RpmVersion(('0', '1.9.17', '5.p1.fc43'))
     repo.export(output_path, pgp_signer, now=fixed_datetime, keep_expanded=True)
     compare_files(output_path / 'sudo-1.9.17-5.p1.fc43.x86_64.rpm', binaries_path / 'sudo-1.9.17-5.p1.fc43.x86_64.rpm')
-    compare_files(output_path / 'repodata/repomd.xml', expected_path / 'repomd.xml', should_populate)
+    compare_xml_files(output_path / 'repodata/repomd.xml', expected_path / 'repomd.xml', REPOMD_NORMALIZE, should_populate)
     compare_files(output_path / 'repodata/primary.xml', expected_path / 'primary.xml', should_populate)
     compare_files(output_path / 'repodata/filelists.xml', expected_path / 'filelists.xml', should_populate)
     compare_files(output_path / 'repodata/other.xml', expected_path / 'other.xml', should_populate)
@@ -59,7 +73,7 @@ def test_two(binaries_path, output_path, expected_path, pgp_signer, fixed_dateti
     repo.export(output_path, pgp_signer, now=fixed_datetime, keep_expanded=True)
     compare_files(output_path / 'sudo-1.9.17-5.p1.fc43.x86_64.rpm', binaries_path / 'sudo-1.9.17-5.p1.fc43.x86_64.rpm')
     compare_files(output_path / 'dhcp-devel-4.4.3-22.fc43.x86_64.rpm', binaries_path / 'dhcp-devel-4.4.3-22.fc43.x86_64.rpm')
-    compare_files(output_path / 'repodata/repomd.xml', expected_path / 'repomd.xml', should_populate)
+    compare_xml_files(output_path / 'repodata/repomd.xml', expected_path / 'repomd.xml', REPOMD_NORMALIZE, should_populate)
     compare_files(output_path / 'repodata/primary.xml', expected_path / 'primary.xml', should_populate)
     compare_files(output_path / 'repodata/filelists.xml', expected_path / 'filelists.xml', should_populate)
     compare_files(output_path / 'repodata/other.xml', expected_path / 'other.xml', should_populate)
--- tests/utils.py
+++ tests/utils.py
@@ -11,6 +11,7 @@ import shutil
 import gzip
 
 from pathlib import Path
+from lxml import etree
 
 from repopulator.util import file_digest
 
@@ -31,6 +32,23 @@ def compare_files(actual: Path, expected: Path, populate_expected: bool=False):
     if not populate_expected:
         assert actual.exists()
         assert hash_file(actual) == hash_file(expected)
+    else:
+        expected.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy2(actual, expected)
+
+
+def _hash_xml_transformed(path: Path, xslt: etree.XSLT|None) -> str:
+    if not path.exists():
+        return ''
+    if xslt is not None:
+        return hashlib.sha256(bytes(xslt(etree.parse(path)))).hexdigest()
+    return hash_file(path)
+
+def compare_xml_files(actual: Path, expected: Path, transform: str|None=None, populate_expected: bool=False):
+    if not populate_expected:
+        assert actual.exists()
+        xslt = etree.XSLT(etree.fromstring(transform.encode('utf-8'))) if transform is not None else None
+        assert _hash_xml_transformed(actual, xslt) == _hash_xml_transformed(expected, xslt)
     else:
         expected.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy2(actual, expected)
